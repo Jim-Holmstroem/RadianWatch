4bead92c7e67eaf834f6ea0c43429f9764384c78
Jim Holmström <jim.holmstroem@gmail.com>
2013-02-27 10:55:49 +0100

    works

--- radianwatch/radianwatch.py
+++ radianwatch/radianwatch.py
@@ -1,26 +1,26 @@
 from __future__ import print_function
 
 from datetime import datetime
+import time
 from sympy import *
 import itertools as it
 import functools as ft
 import re
 from tempfile import NamedTemporaryFile
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import check_call, call, Popen, PIPE
 import os
 import BaseHTTPServer as httpserver
 
-HOSTNAME, PORT = '192.168.1.85', 80
+HOSTNAME, PORT = 'localhost', 80
 
-latex_template = "\\documentclass{{amsart}}\n\
-\\usepackage{{color}} % For invisible frame\n\
-\\usepackage{{amsmath}} % For invisible frame\n\
+latex_template = "\\documentclass{{minimal}}\n\
+\\usepackage{{amsmath}} \n\
 \\begin{{document}}\n\
-    \\thispagestyle{{empty}} % No page numbers\n\
     {equation}\n\
 \\end{{document}}\n".format 
 
-prettier_radians = ft.partial( #\frac{x}{y}pi -> \frac{xpi}{y}
+TMP_LOCATION="/tmp"
+prettier_radians = ft.partial( #\frac{x}{y}\pi -> \frac{x\pi}{y}
     re.sub,
     r"\\frac\{\s*(?P<m>[0-9]+)\s*\}\{\s*(?P<n>[0-9]+)\s*\}\s*\\pi",
     r"\\frac{\g<m>\pi}{\g<n>}"
@@ -31,63 +31,93 @@ remove_loneones = ft.partial( #removes 1\pi which just looks silly, could have f
     r"\\frac{\pi}"
 )
 
-def png_render(filename):
+def png_render(latex_code): #perhaps race conditions, shouldn't be any security issues as long as this file isn't writable
     proc=Popen(
         [
-            "latex2png", 
-            "-g", 
-            "-d", 
-            "12800",
-            str(filename)
+            "pdflatex", #doesn't really behave as a normal program, a little workarounding
+            "-output-directory={tmp}".format(tmp=TMP_LOCATION),
+            "--halt-on-error"
         ],
-        stdout=PIPE,
-        stderr=PIPE,
-        cwd='/tmp'
+        shell=False,
+        stdin=PIPE,
     )
-    proc.wait()
-    stdout, stderr = proc.communicate()
-    print(stdout)
-    get_png_filename = lambda filename: open(filename.replace('.tex','.png')) 
-    #that is how latex2png works, didn't find workaround to specify the outputfile (another tempfile)
-    png_file = get_png_filename(filename)
+    stdout, stderr = proc.communicate(latex_code) #also waits, only nice way to interagate with this ancient pdflatex, since it doesn't behave well.
+    
+    os.system(
+        'pdfcrop {tmp}/texput.pdf {tmp}/texput-crop.pdf'.format(tmp=TMP_LOCATION)
+    ) #pdfcrop doesn't work with Popen, gets: OSError: [Errno 8] Exec format error
+    os.system(
+        'convert -density 600 {tmp}/texput-crop.pdf {tmp}/texput.png'.format(tmp=TMP_LOCATION)
+    ) #didn't work well with Popen :(
+
+    png_file = open('{tmp}/texput.png'.format(tmp=TMP_LOCATION))
     png_data = png_file.read()
     png_file.close()
-    os.remove(png_file.name)
-    return "" #png_data
+    return png_data
 
 def latex2png(equation):
-    with NamedTemporaryFile(suffix='.tex', delete=False) as f:
-        latex_code = latex_template(
-            equation=remove_loneones(
-                prettier_radians(
-                    latex(
-                        equation,
-                        mode='inline'
-                    )
+    latex_code = latex_template(
+        equation=remove_loneones(
+            prettier_radians(
+                latex(
+                    equation,
+                    mode='inline'
                 )
             )
         )
-        f.file.write(latex_code)
-        png_data = png_render(f.name)
-    return png_data
-
-time = datetime.time(datetime.now())
-
-print(
-    latex2png(
-        equation=(
-	    2*((3-time.hour)%24)*pi/12, #\in [0,4pi]
-	    2*((15-time.minute)%60)*pi/60, #\in [0,2pi]
-	    2*((15-time.second)%60)*pi/60 #\in [0,2pi]
-        )#TODO change basis to be more radian like (start at x for example)
     )
-)
+    png_data = png_render(latex_code)
+    return png_data
 
 class Handler(httpserver.BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'image/png')
+        self.send_response(
+            200
+        )
+        self.send_header(
+            'Content-type', 
+            'image/png'
+        )
+        self.send_header(
+            'Content-disposition', 
+            'filename=radianwatch.png'
+        )
         self.end_headers()
+        
+        time = datetime.time(
+            datetime.now()
+        )
+        png_data = latex2png(
+            equation=(
+                2*((3-time.hour)%24)*pi/12, #\in [0,4pi]
+                2*((15-time.minute)%60)*pi/60, #\in [0,2pi]
+                2*((15-time.second)%60)*pi/60 #\in [0,2pi]
+            )
+        )
+        self.wfile.write(png_data)
 
-
+if __name__ == "__main__":
+    httpd = httpserver.HTTPServer(
+        (HOSTNAME, PORT),
+        Handler
+    )
+    print(
+        time.asctime(), 
+        "Server start - {name}:{port}".format(
+            name=HOSTNAME, 
+            port=PORT
+        )
+    )
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print(
+        time.asctime(), 
+        "Server stops - {name}:{port}".format(
+            name=HOSTNAME, 
+            port=PORT
+        )
+    )
 
